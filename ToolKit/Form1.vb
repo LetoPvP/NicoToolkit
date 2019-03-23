3490ce4f8612ab878f140a83f2ff6687f45cd717
Nicolás Elvira Antunes <nico.elvira1992@gmail.com>
2019-03-23 23:42:58 +0100

    Compiled Add
    New route compiled .exe

--- ToolKit/Form1.vb
+++ ToolKit/Form1.vb
@@ -266,7 +266,6 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
-        subinacl / subkeyreg HKEY_LOCAL_MACHINE /grant=administrators=f /grant=system=f
         My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\Wow6432Node", True).CreateSubKey("NicoToolkit")
 
     End Sub
